a27e88133d44d418678fa1aa899db3be161f42b5
Silas Michael Knight <silasfelinus@gmail.com>
2020-12-06 21:33:11 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,9 @@
 class Meal
+  attr_accessor :customer, :waiter, :total, :tip
 
-end
+  def initialize(customer, waiter, total, tip)
+    @customer = customer
+    @waiter = waiter
+    @total = total
+    @tip = tip
+  end
