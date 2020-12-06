0b7b3e5d4dd869ec13de3d7b53f325ce9063f446
Silas Michael Knight <silasfelinus@gmail.com>
2020-12-06 21:48:47 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -6,10 +6,20 @@ class Waiter
     @name = name
     @years_experience = years_experience
     @@all << self
+    @customers = []
+    @meals = []
   end
 
   def self.all
     @@all
   end
+
+  def new_meal(customer, total, tip)
+    new_meal = Meal.new(customer, self, total, tip)
+    @meals << new_meal
+    @customers << customer
+    new_meal
+  end
+
 
 end
