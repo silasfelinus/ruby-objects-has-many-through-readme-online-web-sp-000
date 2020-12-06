ef20712239ebf98efdaed055bfb3483d2071ea00
Silas Michael Knight <silasfelinus@gmail.com>
2020-12-06 21:53:04 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,5 +1,5 @@
 class Waiter
-  attr_accessor :name, :years_experience
+  attr_accessor :name, :years_experience, :best_tip, :best_tipper
   @@all = []
 
   def initialize(name, years_experience)
@@ -8,6 +8,9 @@ class Waiter
     @@all << self
     @customers = []
     @meals = []
+    @best_tip = 0
+    @best_customer = ""
+
   end
 
   def self.all
@@ -18,7 +21,15 @@ class Waiter
     new_meal = Meal.new(customer, self, total, tip)
     @meals << new_meal
     @customers << customer
+    if tip > best_tip
+      best_tip = tip
+      best_tipper = customer
+    end
     new_meal
+  end
+
+  def meals
+    @meals
   end
 
 
