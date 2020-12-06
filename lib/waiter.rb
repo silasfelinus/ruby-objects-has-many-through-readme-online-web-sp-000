57941e3c212b2df4a17511a4e1fdfdeb4077bb96
Silas Michael Knight <silasfelinus@gmail.com>
2020-12-06 21:38:08 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,14 @@
 class Waiter
+  attr_accessor :name, :years_experience
+  @@all = []
 
+  def initialize(name, years_experience)
+    @name = name
+    @years_experience = years_experience
+    @@all << self
+  end
+
+  def self.all
+    @@all 
+    
 end
